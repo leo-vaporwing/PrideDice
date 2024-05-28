878957217051fa4b0132c9a32f3d21fe3b3bddf4
leo-vaporwing <leo.vaporwing@gmail.com>
2024-05-29 01:58:17 +0200

    MOOOAAAR DIIIIICCEEE!!!!

--- pride.js
+++ pride.js
@@ -16,27 +16,9 @@ Hooks.once('diceSoNiceReady', (dice3d) => {
 	dice3d.addDicePreset({
 		type: "d8",
 		labels: "",
-		modelFile: "modules/pride-dice/wcube_d8.glb",
+		modelFile: "modules/pride-dice/bi-pride/bi_d8.glb",
 		system: "bi-pride"
 	});
-	dice3d.addDicePreset({
-		type: "d10",
-		labels: "",
-		modelFile: "modules/pride-dice/wcube_d10.glb",
-		system: "bi-pride"
-	});
-	dice3d.addDicePreset({
-		type: "d100",
-		labels: "",
-		modelFile: "modules/pride-dice/wcube_d100.glb",
-		system: "bi-pride"
-	});
-	dice3d.addDicePreset({
-		type: "d12",
-		labels: "",
-		modelFile: "modules/pride-dice/wcube_d12.glb",
-		system: "bi-pride"
-	});	
 	dice3d.addDicePreset({
 		type: "d20",
 		labels: "",
@@ -60,27 +42,9 @@ Hooks.once('diceSoNiceReady', (dice3d) => {
 	dice3d.addDicePreset({
 		type: "d8",
 		labels: "",
-		modelFile: "modules/pride-dice/wcube_d8.glb",
+		modelFile: "modules/pride-dice/trans-pride/trans_d8.glb",
 		system: "trans-pride"
-	});
-	dice3d.addDicePreset({
-		type: "d10",
-		labels: "",
-		modelFile: "modules/pride-dice/wcube_d10.glb",
-		system: "trans-pride"
-	});
-	dice3d.addDicePreset({
-		type: "d100",
-		labels: "",
-		modelFile: "modules/pride-dice/wcube_d100.glb",
-		system: "trans-pride"
-	});
-	dice3d.addDicePreset({
-		type: "d12",
-		labels: "",
-		modelFile: "modules/pride-dice/wcube_d12.glb",
-		system: "trans-pride"
-	});	
+	});		
 	dice3d.addDicePreset({
 		type: "d20",
 		labels: "",
