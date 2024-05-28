67fadabebe0f840ab72794a5ac61b95153b09d4d
leo-vaporwing <leo.vaporwing@gmail.com>
2024-05-28 21:31:10 +0200

    added trans proof of concept

--- pride.js
+++ pride.js
@@ -1,5 +1,6 @@
 Hooks.once('diceSoNiceReady', (dice3d) => {
-	dice3d.addSystem({id: "bi-pride", name: "Aperture Science"}, false);
+	// Bi Pride
+	dice3d.addSystem({id: "bi-pride", name: "Bi Pride"}, false);
 	dice3d.addDicePreset({
 		type: "d2",
 		labels: "",
@@ -47,5 +48,55 @@ Hooks.once('diceSoNiceReady', (dice3d) => {
 		labels: "",
 		modelFile: "modules/pride-dice/bi-pride/bi_d20.glb",
 		system: "bi-pride"
+	});
+	// Trans dice
+	dice3d.addSystem({id: "trans-pride", name: "Trans Pride"}, false);
+	dice3d.addDicePreset({
+		type: "d2",
+		labels: "",
+		modelFile: "modules/pride-dice/wcube_d2.glb",
+		system: "trans-pride"
+	});
+	dice3d.addDicePreset({
+		type: "d4",
+		labels: "",
+		modelFile: "modules/pride-dice/wcube_d4.glb",
+		system: "trans-pride"
+	});
+	dice3d.addDicePreset({
+		type: "d6",
+		labels: "",
+		modelFile: "modules/pride-dice/wcube_d6.glb",
+		system: "trans-pride"
+	});
+	dice3d.addDicePreset({
+		type: "d8",
+		labels: "",
+		modelFile: "modules/pride-dice/wcube_d8.glb",
+		system: "trans-pride"
+	});
+	dice3d.addDicePreset({
+		type: "d10",
+		labels: "",
+		modelFile: "modules/pride-dice/wcube_d10.glb",
+		system: "trans-pride"
+	});
+	dice3d.addDicePreset({
+		type: "d100",
+		labels: "",
+		modelFile: "modules/pride-dice/wcube_d100.glb",
+		system: "trans-pride"
+	});
+	dice3d.addDicePreset({
+		type: "d12",
+		labels: "",
+		modelFile: "modules/pride-dice/wcube_d12.glb",
+		system: "trans-pride"
+	});	
+	dice3d.addDicePreset({
+		type: "d20",
+		labels: "",
+		modelFile: "modules/pride-dice/trans-pride/trans_d20.glb",
+		system: "trans-pride"
 	});
 });
